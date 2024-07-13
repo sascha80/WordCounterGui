b131a8c014c9bc487f7fcd78270e287827aba03b
Sascha Hinkelmann <94072200+sascha80@users.noreply.github.com>
2024-07-13 15:40:29 +0200

    Update FileUtils.java

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,5 +1,16 @@
 package utils;
 
-public class FileUtils {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+public class FileUtils {
+    public static String readFile(File file) {
+        try {
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
